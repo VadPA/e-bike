6f3872a04f4d5a1a4416f7e5623e2932aafe20ab
vadimP74 <mydom74@gmail.com>
2023-10-04 17:34:29 -0500

    added other blocks

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,26 @@
-import mobileNav from './modules/mobile-nav.js';
-mobileNav();
+$(function () {
+  $('.bike__slider, .slider__items').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+    autoplay: true,
+    speed: 2000,
+    cssEase: 'linear',
+  }),
+    $('.price-slider__items').slick({
+      arrows: false,
+      dots: true,
+      fade: true,
+      speed: 1000,
+      cssEase: 'linear',
+    }),
+    $('.menu, .bike__column, .form').on('click', 'a', function (event) {
+      event.preventDefault()
+      var id = $(this).attr('href'),
+        top = $(id).offset().top
+      $('body,html').animate({ scrollTop: top }, 1000)
+    })
+})
+
+// import mobileNav from './modules/mobile-nav.js';
+// mobileNav();
